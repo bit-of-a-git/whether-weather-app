0df7c270cb374d9acba19939108df1d1964c3a0f
doc335 <92552641+bit-of-a-git@users.noreply.github.com>
2024-05-08 23:00:21 +0100

    forgot to commit moving functions to js/utilities folder

--- js/utilities/capitaliseCityName.js
+++ js/utilities/capitaliseCityName.js
@@ -2,7 +2,9 @@ const capitaliseCityName = (city) => {
 
   const cityName = city.replace("_daily", "");
 
+  // First splits the city value into words using '_', then capitalises the first letter of each word
   const cityNameSplit = cityName.split('_').map(word => word.charAt(0).toUpperCase() + word.slice(1));
 
+  // Returns the words as a single string joined by spaces
   return cityNameSplit.join(" ");
 }
--- js/utilities/getImageSource.js
+++ js/utilities/getImageSource.js
@@ -1,27 +1,31 @@
-  // Function to set the weather image from the code
+  // This function fetches the weather image using a map, and checking whether the weatherCode
+  // passed is found in one of the keys in the map
   function getImageSource(weatherCode) {
     for (const key in weatherImageMap) {
+      // Gets the start and the end values by splitting using "-"
       const [start, end] = key.split("-").map(Number);
       if (start <= weatherCode && weatherCode <= end) {
         return weatherImageMap[key];
       }
     }
-  // Default to sun image if no match found
+  // Defaults to sun image if no match found
   return "/images/sun.png";
   }
 
+// For single numbers, the value is doubled with a dash between it
+// so it can serve as the end value too 
 const weatherImageMap = {
-    0: "/images/sun.png",
-    "1-3": "/images/clear-sky.png",
-    "45-48": "/images/fog.png",
-    "51-55": "/images/drizzle.png",
-    "56-57": "/images/freezingDrizzle.png",
-    "61-65": "/images/rainHeavy.png",
-    "66-67": "/images/freezingRain.png",
-    "71-75": "/images/snowfall.png",
-    77: "/images/snowGrains.png",
-    "80-82": "/images/heavyRain.png",
-    "85-86": "/images/snowShowers.png",
-    95: "/images/thunderstorm.png",
-    "96-99": "/images/thunderAndHail.png"
-  };
+  "0-0": "/images/sun.png",
+  "1-3": "/images/clear-sky.png",
+  "45-48": "/images/fog.png",
+  "51-55": "/images/drizzle.png",
+  "56-57": "/images/freezingDrizzle.png",
+  "61-65": "/images/rainHeavy.png",
+  "66-67": "/images/freezingRain.png",
+  "71-75": "/images/snowfall.png",
+  "77-77": "/images/snowGrains.png",
+  "80-82": "/images/heavyRain.png",
+  "85-86": "/images/snowShowers.png",
+  "95-95": "/images/thunderstorm.png",
+  "96-99": "/images/thunderAndHail.png"
+};
